de39960bbbf6c0112864ae369a94212bc337605c
roshii <roshii@riseup.net>
2018-06-27 14:50:16 +0200

    remove old files, including new

---
+++ bitcoin/0.11.2/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" == ""$NODECHAIN"-cli" || "$1" == ""$NODECHAIN"-tx" || "$1" == ""$NODECHAIN"d" || "$1" == "test_"$NODECHAIN"" ]]; then
+	mkdir -p "$NODEDATA"
+
+	if [[ ! -s "$NODEDATA/$NODECHAIN.conf" ]]; then
+		cat <<-EOF > "$NODEDATA/$NODECHAIN.conf"
+		printtoconsole=1
+		rpcallowip=::/0
+		rpcpassword=admin
+		rpcuser=admin
+		EOF
+		chown "$NODECHAIN:$NODECHAIN" "$NODEDATA/$NODECHAIN.conf"
+	fi
+
+	# ensure correct ownership and linking of data directory
+	# we do not update group ownership here, in case users want to mount
+	# a host directory and still retain access to it
+	chown -R "$NODECHAIN" "$NODEDATA"
+	ln -sfn "$NODEDATA" "/home/$NODECHAIN/.$NODECHAIN"
+	chown -h "$NODECHAIN:$NODECHAIN" "/home/$NODECHAIN/.$NODECHAIN"
+
+	exec gosu "$NODECHAIN" "$@"
+fi
+
+exec "$@"
