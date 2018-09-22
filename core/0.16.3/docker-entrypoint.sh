45f304e1b60879b8007be53eb56ae61c2907dbef
roshii <roshii@riseup.net>
2018-09-22 14:21:37 +0200

    Switch to Litecoin

--- core/0.16.3/docker-entrypoint.sh
+++ core/0.16.3/docker-entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
-	mkdir -p "$BITCOIN_DATA"
+if [[ "$1" == "litecoin-cli" || "$1" == "litecoin-tx" || "$1" == "litecoind" || "$1" == "test_litecoin" ]]; then
+	mkdir -p "$LITECOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+	if [[ ! -s "$LITECOIN_DATA/litecoin.conf" ]]; then
+		cat <<-EOF > "$LITECOIN_DATA/litecoin.conf"
 		printtoconsole=1
 		rpcallowip=::/0
-		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+		rpcpassword=${LITECOIN_RPC_PASSWORD:-password}
+		rpcuser=${LITECOIN_RPC_USER:-litecoin}
 		EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
+		chown litecoin:litecoin "$LITECOIN_DATA/litecoin.conf"
 	fi
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
-	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
+	chown -R litecoin "$LITECOIN_DATA"
+	ln -sfn "$LITECOIN_DATA" /home/litecoin/.litecoin
+	chown -h litecoin:litecoin /home/litecoin/.litecoin
 
-	exec gosu bitcoin "$@"
+	exec gosu litecoin "$@"
 fi
 
 exec "$@"
